7dc98db6225d151cba7878e3644455318ad50d47
sadraliev <a.sadraliev@gmail.com>
2023-07-31 22:14:49 +0600

    chore: initialization react app by CRA and prepared design system

---
+++ src/components/ChatItemList/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+interface ChatItemListProps {}
+
+export const ChatItemList: FC<ChatItemListProps> = props => {
+  return <div>ChatItemList</div>;
+};
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,16 @@
-import React, {FC} from "react";
-import {IPage} from "../../interface/page";
+import React, { FC } from "react";
+import { IPage } from "../../interface/page";
+import { Avatar } from "../../components/Avatar";
 
 export const PageIndex: FC<IPage> = (props: IPage) => {
-    const { title } = props;
+  const { title } = props;
 
-    return (
-        <>
-
-        </>
-    )
-}
+  return (
+    <div className="container">
+      <div className="chats">
+        <Avatar />
+      </div>
+      <div className="message"></div>
+    </div>
+  );
+};
